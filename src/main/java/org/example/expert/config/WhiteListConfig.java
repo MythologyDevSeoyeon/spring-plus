ce9f21d52a376468bfc5ceeb95b021f5a9f4e816
Seoyeon <dev_seoyeon@mythologyteams.com>
2025-03-14 10:29:42 +0900

    Lv2: 9. Spring Security

---
+++ src/main/java/org/example/expert/config/WhiteListConfig.java
@@ -0,0 +1,18 @@
+package org.example.expert.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class WhiteListConfig {
+
+    @Bean
+    public List<String> whiteList(){
+        return List.of(
+                "/auth/signup",
+                "/auth/signin"
+        );
+    }
+}

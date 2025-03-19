252f5249ecd50599e506e5ad57fea557760cce1e
Seoyeon <dev_seoyeon@mythologyteams.com>
2025-03-19 23:02:11 +0900

    Fix: healthcheck 시큐리티 설정 추가

--- src/main/java/org/example/expert/domain/common/controller/HealthCheckController.java
+++ src/main/java/org/example/expert/domain/common/controller/HealthCheckController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthCheckController {
 
-    @GetMapping("api/health")
+    @GetMapping("/api/health")
     public ResponseEntity<String> healthCheck() {
         return ResponseEntity.ok("어플리케이션이 실행 중 입니다.");
     }
